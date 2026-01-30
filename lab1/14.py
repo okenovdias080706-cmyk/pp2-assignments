e2ae2da29ebf9acb34b6e60a53c72f9706868f35
OkenovDias <okenovdias080706@gmail.com>
2026-01-30 23:35:59 +0500

    first upload

---
+++ lab1/14.py
@@ -0,0 +1,3 @@
+x=input()
+y=int(input())
+print(f"Hello, {x}. You are {y} years old.")
---
+++ lab1/20.py
@@ -0,0 +1,8 @@
+x=int(input())
+y=int(input())
+if y>x:
+    print(y)
+elif x>y:
+    print(x)
+else:
+    print("equal")
