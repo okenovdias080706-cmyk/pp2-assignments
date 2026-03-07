14a4e39fd33a7e4879db1520945cb7defc6ebeff
OkenovDias <okenovdias080706@gmail.com>
2026-03-07 12:36:10 +0500

    first upload

--- prac5/3.py
+++ prac5/3.py
@@ -1,6 +1,46 @@
+#1
 import re
 
 text = "I have 2 apples"
 cod=re.search("\\d", text)
 if cod:
     print("Found a number:", cod.group())
+
+#2
+import re
+
+text = "My email is test@mail.com"
+x = re.search("@", text)
+print(x)
+
+#3
+import re
+
+text = "apple,banana,orange"
+x = re.split(",", text)
+
+print(x)
+
+#4
+import re
+
+text = "I like cats"
+x = re.sub("cats", "dogs", text)
+
+print(x)
+
+#5
+import re
+
+text = "world Hello"
+
+x = re.match("Hello", text)
+print(x)
+
+#6
+import re
+
+text = "HELLO"
+
+x = re.search("hello", text, re.IGNORECASE)
+print(x)
